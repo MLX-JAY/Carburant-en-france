c2a1827481d3347747deea35e7e8d338f4491847
Thenuxshan <anutajant@gmail.com>
2026-04-29 21:15:23 +0200

    j'ai ajouté les départements

---
+++ include/fonction.inc.php
@@ -0,0 +1,148 @@
+<?php
+
+$regionsDepartements = [
+    0 => [  // Bretagne
+        ['id' => '22', 'nom' => 'Côtes-d\'Armor'],
+        ['id' => '29', 'nom' => 'Finistère'],
+        ['id' => '35', 'nom' => 'Ille-et-Vilaine'],
+        ['id' => '56', 'nom' => 'Morbihan'],
+    ],
+    1 => [  // Normandie
+        ['id' => '14', 'nom' => 'Calvados'],
+        ['id' => '27', 'nom' => 'Eure'],
+        ['id' => '50', 'nom' => 'Manche'],
+        ['id' => '61', 'nom' => 'Orne'],
+        ['id' => '76', 'nom' => 'Seine-Maritime'],
+    ],
+    2 => [  // Hauts-de-France
+        ['id' => '59', 'nom' => 'Nord'],
+        ['id' => '62', 'nom' => 'Pas-de-Calais'],
+        ['id' => '80', 'nom' => 'Somme'],
+        ['id' => '60', 'nom' => 'Oise'],
+        ['id' => '02', 'nom' => 'Aisne'],
+    ],
+    3 => [  // Grand Est
+        ['id' => '08', 'nom' => 'Ardennes'],
+        ['id' => '10', 'nom' => 'Aube'],
+        ['id' => '51', 'nom' => 'Marne'],
+        ['id' => '52', 'nom' => 'Haute-Marne'],
+        ['id' => '54', 'nom' => 'Meurthe-et-Moselle'],
+        ['id' => '55', 'nom' => 'Meuse'],
+        ['id' => '57', 'nom' => 'Moselle'],
+        ['id' => '67', 'nom' => 'Bas-Rhin'],
+        ['id' => '68', 'nom' => 'Haut-Rhin'],
+        ['id' => '88', 'nom' => 'Vosges'],
+    ],
+    4 => [  // Île-de-France
+        ['id' => '75', 'nom' => 'Paris'],
+        ['id' => '77', 'nom' => 'Seine-et-Marne'],
+        ['id' => '78', 'nom' => 'Yvelines'],
+        ['id' => '91', 'nom' => 'Essonne'],
+        ['id' => '92', 'nom' => 'Hauts-de-Seine'],
+        ['id' => '93', 'nom' => 'Seine-Saint-Denis'],
+        ['id' => '94', 'nom' => 'Val-de-Marne'],
+        ['id' => '95', 'nom' => 'Val-d\'Oise'],
+    ],
+    5 => [  // Bourgogne
+        ['id' => '21', 'nom' => 'Côte-d\'Or'],
+        ['id' => '58', 'nom' => 'Nièvre'],
+        ['id' => '71', 'nom' => 'Saône-et-Loire'],
+        ['id' => '89', 'nom' => 'Yonne'],
+        ['id' => '63', 'nom' => 'Puy-de-Dôme'],
+    ],
+    6 => [  // Val-de-Loire
+        ['id' => '18', 'nom' => 'Cher'],
+        ['id' => '28', 'nom' => 'Eure-et-Loir'],
+        ['id' => '36', 'nom' => 'Indre'],
+        ['id' => '37', 'nom' => 'Indre-et-Loire'],
+        ['id' => '41', 'nom' => 'Loir-et-Cher'],
+        ['id' => '45', 'nom' => 'Loiret'],
+        ['id' => '72', 'nom' => 'Sarthe'],
+        ['id' => '86', 'nom' => 'Vienne'],
+    ],
+    7 => [  // Pays de la Loire
+        ['id' => '44', 'nom' => 'Loire-Atlantique'],
+        ['id' => '49', 'nom' => 'Maine-et-Loire'],
+        ['id' => '53', 'nom' => 'Mayenne'],
+        ['id' => '72', 'nom' => 'Sarthe'],
+        ['id' => '85', 'nom' => 'Vendée'],
+    ],
+    8 => [  // Nouvelle-Aquitaine
+        ['id' => '16', 'nom' => 'Charente'],
+        ['id' => '17', 'nom' => 'Charente-Maritime'],
+        ['id' => '19', 'nom' => 'Corrèze'],
+        ['id' => '24', 'nom' => 'Dordogne'],
+        ['id' => '33', 'nom' => 'Gironde'],
+        ['id' => '40', 'nom' => 'Landes'],
+        ['id' => '47', 'nom' => 'Lot-et-Garonne'],
+        ['id' => '64', 'nom' => 'Pyrénées-Atlantiques'],
+        ['id' => '79', 'nom' => 'Deux-Sèvres'],
+        ['id' => '87', 'nom' => 'Haute-Vienne'],
+        ['id' => '86', 'nom' => 'Vienne'],
+    ],
+    9 => [  // Occitanie
+        ['id' => '09', 'nom' => 'Ariège'],
+        ['id' => '11', 'nom' => 'Aude'],
+        ['id' => '12', 'nom' => 'Aveyron'],
+        ['id' => '30', 'nom' => 'Gard'],
+        ['id' => '31', 'nom' => 'Haute-Garonne'],
+        ['id' => '32', 'nom' => 'Gers'],
+        ['id' => '34', 'nom' => 'Hérault'],
+        ['id' => '46', 'nom' => 'Lot'],
+        ['id' => '48', 'nom' => 'Lozère'],
+        ['id' => '65', 'nom' => 'Hautes-Pyrénées'],
+        ['id' => '66', 'nom' => 'Pyrénées-Orientales'],
+        ['id' => '81', 'nom' => 'Tarn'],
+        ['id' => '82', 'nom' => 'Tarn-et-Garonne'],
+    ],
+    10 => [  // Côte d'Azur
+        ['id' => '04', 'nom' => 'Alpes-de-Haute-Provence'],
+        ['id' => '05', 'nom' => 'Hautes-Alpes'],
+        ['id' => '06', 'nom' => 'Alpes-Maritimes'],
+        ['id' => '83', 'nom' => 'Var'],
+        ['id' => '84', 'nom' => 'Vaucluse'],
+    ],
+    11 => [  // Auvergne
+        ['id' => '03', 'nom' => 'Allier'],
+        ['id' => '15', 'nom' => 'Cantal'],
+        ['id' => '43', 'nom' => 'Haute-Loire'],
+        ['id' => '63', 'nom' => 'Puy-de-Dôme'],
+    ],
+    12 => [  // Corse
+        ['id' => '2A', 'nom' => 'Corse-du-Sud'],
+        ['id' => '2B', 'nom' => 'Haute-Corse'],
+    ],
+];
+
+$regionsNoms = [
+    0 => 'Bretagne',
+    1 => 'Normandie',
+    2 => 'Hauts-de-France',
+    3 => 'Grand Est',
+    4 => 'Île-de-France',
+    5 => 'Bourgogne',
+    6 => 'Val-de-Loire',
+    7 => 'Pays de la Loire',
+    8 => 'Nouvelle-Aquitaine',
+    9 => 'Occitanie',
+    10 => 'Côte d\'Azur',
+    11 => 'Auvergne',
+    12 => 'Corse',
+];
+
+function afficherDepartements(array $departements): void {
+    global $style, $lang;
+    
+    echo '<div class="cartes-departements">';
+    
+    foreach ($departements as $dept) {
+        echo '<article class="carte-departement" id="departements">';
+        echo '<span class="departement-numero">' . $dept['id'] . '</span>';
+        echo '<h3 class="departement-nom">' . htmlspecialchars($dept['nom']) . '</h3>';
+        echo '<a href="?dep=' . $dept['id'] . '&lang=' . $lang . '&style=' . $style . '" class="bouton-departement">Voir les villes</a>';
+        echo '</article>';
+    }
+    
+    echo '</div>';
+}
+?>
